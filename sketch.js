f53caa7c01681c882c2fa8a9359ac69c1b366799
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-10-11 15:55:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,27 @@
-var bullet,speed,weight;
-var wall,thickness
+var car,wall;
+var speed, weight
 function setup() {
-  createCanvas(800,400);
-  speed=random(30,50)
-  weight=random(400,1500)
-  createSprite(400, 200, 50, 50);
-  bullet=createSprite(50,200,50,50);
-  wall=createSprite(400,200,60,height/2)
-  wall.shapeColor(black)
-  bullet.velocityX = speed;
+  createCanvas(1600,400);
+  speed = random(55,90)
+  weight = random(400,1500)
+car = createSprite(50,200,50,50);
+wall = createSprite(1500,200,50,50);
+  car.velocityX = speed;
+  if(speed< 100){
+    car.shapeColor(0,255,0);
+  } 
+
+  if(speed = (100-180)){
+    car.shapeColor(230,230,0);
+  } 
+
+  if(speed > 180){
+    car.shapeColor(255,0,0);
+  } 
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255); 
+  car.collide(wall);
   drawSprites();
-  bullet.collide(wall);
 }
