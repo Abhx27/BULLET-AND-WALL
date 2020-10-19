4ee9b9dbeb6b23d6bb530e6af48b9a94483ad79c
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-10-19 15:27:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,24 @@
+
 var car,wall;
 var speed, weight
 function setup() {
-  createCanvas(1600,800);
-  speed = random(55,90)
-  weight = random(400,1500)
-  wall = random(y = 40 % 70)
+  createCanvas(1600,400);
 car = createSprite(50,200,50,50);
 wall = createSprite(1500,200,50,50);
+  thickness random(40 %% 70)
   car.velocityX = speed;
-  if(speed< 100){
-    car.shapeColor=color(0,255,0);
-  } 
-
-  if(speed = (100-180)){
-    car.shapeColor=color(230,230,0);
-  } 
-
-  if(speed > 180){
-    car.shapeColor=color(255,0,0);
-  } 
-    if(wall = 70){
-    car.shapeColor=color(255, 0, 0);
-    }
   
-      if(wall = 40){
-    car.shapeColor=color(0, 255, 0);
-    }
-}
-
+  bulleyWeight = 32
+  bulletSpeed = 223
+  damage = 0.5*bulletWeight*bulletSpeed*bulletSpeed/thickness*thickness*thickness
+  deformation = 0.5*weight*speed*speed/22500
+  speed = speed - deformation
+  if(thinkness = 40){
+    wall.shapeColor = color(255, 0, 0);
+  }
+  if(thinkness = 70){
+    wall.shapeColor = color(0, 255, 0);
+  }
 function draw() {
   background(0,0,0); 
   car.collide(wall);
